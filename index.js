ddd60928fd89372a2d847bb133d7d4893af31e60
Zainab Agboola <zagboola@nyit.edu>
2019-04-08 21:59:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your code in this file!
+function scuberGreetingForFeet(feet){
+if (feet <= 400){
+ return 'This one is on me!';
+}
+if (feet > 2500){
+  return 'No can do.';
+}
+if (feet >= 2000 ){
+  return 'I will gladly take your thirty bucks.';
+}
+}
+function ternaryCheckCity(city){
+  if (city === "NYC"){
+  return "Ok, sounds good.";
+}
+else{
+  return "No go.";
+}
+// function switchOnCharmFromTip(generous){}
+// function switchOnCharmFromTip(notAsGenerous){}
+// function switchOnCharmFromTip(){}
+ switch (response){
+       case 0:  if (tip === 'generous'){
+       return"Thank you so much." ;}
+ break;
+       case 1: if (tip === 'notAsGenerous'){
+       return "Thank you.";}
+ break;
+       case 2: tip === '';
+       return "Bye";
+}
+}
+
+
+// }
+// function switchOnCharmFromTip(tip){
+//     if (tip === 'generous'){
+//       return "Thank you so much." ;
+//     }
+//     else if (tip === 'notAsGenerous'){
+//       return "Thank you.";
+//     }
+//     else(){
+//       return "Bye";
+//     }
